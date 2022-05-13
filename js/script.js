fa51704f5548ef3855b1a923fbbaacbaa3a148cf
Gabry17 <gbianchi528@gmail.com>
2022-05-13 14:39:13 +0200

    generato 10 li con email random con api

--- js/script.js
+++ js/script.js
@@ -1,17 +1,14 @@
 const app = new Vue({
     el:'#root',
     data:{
-        email: ''
+        email: []
     },
-    methods:{
-        generateEmail(){
-            for(let i = 0; i <= 9; i++){
-                axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-                .then((element) => {
-                    this.email = element.data.response;
-                    console.log(this.email);
-                })
-            }
+    created(){
+        for(let i = 0; i <= 9; i++){
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then((element) => {
+                this.email.push(element.data.response);
+            })
         }
     }
 })
